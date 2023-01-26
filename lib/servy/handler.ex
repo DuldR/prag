b7a040469b06b910721a5da61e45cf8369a33063
Garrett <gnjulaka@gmail.com>
2023-01-26 13:41:05 -0600

    Case vs multi functions clauses

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -47,6 +47,41 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "T S P"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do 
+      Path.expand("../../pages", __DIR__)
+      |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 500, resp_body: "File not found"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
+  # def route(%{method: "GET", path: "/about"} = conv) do 
+  #   file =
+  #     Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+
+  #   case File.read(file) do
+  #     {:ok, content} -> 
+  #   %{ conv | status: 200, resp_body: content}
+  #     {:error, :enoent} ->
+  #   %{ conv | status: 500, resp_body: "File not found"}
+  #     {:error, reason} ->
+  #   %{ conv | status: 500, resp_body: "File error: #{reason}"}
+  #   end
+
+  # end
+
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
@@ -114,6 +149,17 @@ IO.puts(response)
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*

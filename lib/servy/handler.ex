7c1dace0c1cde0e79781f13b7fae8188f026795c
Garrett <gnjulaka@gmail.com>
2023-02-28 22:52:02 -0600

    Chap 24 complete

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -21,6 +21,14 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  def route(%Conv{method: "POST", path: "/pledges"} = conv) do
+    Servy.PledgeController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/pledges"} = conv) do
+    Servy.PledgeController.index(conv)
+  end
+
   def route(%Conv{method: "GET", path: "/kaboom"} = conv) do
     raise "Kaboom!"
   end
